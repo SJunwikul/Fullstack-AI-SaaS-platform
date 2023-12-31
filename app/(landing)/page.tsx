0798a049b05905f3bf4b07959c687c14107a8f3f
Jonan <84633272+SJunwikul@users.noreply.github.com>
2023-12-31 16:08:43 -0500

    Final

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+import { LandingContent } from "@/components/landing-content";
+import { LandingHero } from "@/components/landing-hero";
+import { Button } from "@/components/ui/button";
+import { LandingNavbar } from "@/components/landing-navbar";
+
+const LandingPage = () => {
+  return (
+    <div className="h-full">
+      <LandingNavbar />
+      <LandingHero />
+      <LandingContent />
+    </div>
+  );
+};
+
+export default LandingPage;
---
+++ app/api/image/route.ts
@@ -0,0 +1,54 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs";
+import { Configuration, OpenAIApi } from "openai";
+import { increaseApiLimit, checkApiLimit } from "@/lib/api-limit";
+import { checkSubscription } from "@/lib/subscription";
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { prompt, amount = 1, resolution = "512x512" } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!configuration.apiKey) {
+      return new NextResponse("OpenAI API Key not configured", { status: 500 });
+    }
+
+    if (!prompt) {
+      return new NextResponse("Prompt not provided", { status: 400 });
+    }
+    if (!amount) {
+      return new NextResponse("amount not provided", { status: 400 });
+    }
+    if (!resolution) {
+      return new NextResponse("resolution not provided", { status: 400 });
+    }
+    const freeTrial = await checkApiLimit();
+    const isPro = await checkSubscription();
+
+    if (!freeTrial && !isPro) {
+      return new NextResponse("API Limit Exceeded", { status: 403 });
+    }
+    const response = await openai.createImage({
+      prompt,
+      n: parseInt(amount, 10),
+      size: resolution,
+    });
+    if (!isPro) {
+      await increaseApiLimit();
+    }
+    return NextResponse.json(response.data.data);
+  } catch (error) {
+    console.log("[IMAGE_ERROR]", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
