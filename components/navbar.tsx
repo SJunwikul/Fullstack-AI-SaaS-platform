79340d373ba9f430b8ba36b7411604078c21ec5c
Jonan <84633272+SJunwikul@users.noreply.github.com>
2023-12-31 16:13:06 -0500

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = async () => {
 
   return (
     <div className="flex items-center p-4">
-      <MobileSidebar apiLimitCount={apiLimitCount} />
+      <MobileSidebar isPro={isPro} apiLimitCount={apiLimitCount} />
       <div className="flex w-full justify-end">
         <UserButton />
       </div>
